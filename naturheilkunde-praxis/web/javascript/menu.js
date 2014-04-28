4456f0d045c77d1a3d911b48ba57879b6ed47778
Jörg Eisfeld <github@jeisfeld.de>
2014-04-29 00:38:08 +0200

    Resolution improvements on small devices

--- naturheilkunde-praxis/web/javascript/menu.js
+++ naturheilkunde-praxis/web/javascript/menu.js
@@ -1,6 +1,7 @@
 function adjustStyle(width, height) {
 	width = parseInt(width);
 	height = parseInt(height);
+
 	// for left menu
 	if (height < 330) {
 		$('.menuleft hr').css('height', '4px');
@@ -9,14 +10,38 @@ function adjustStyle(width, height) {
 		$('.menuleft hr').css('height', '36px');
 		$('.menuleft a').css('margin-bottom', '5px');
 	}
+	if (width < 130) {
+		$('.menuleft').css('left', '5px');
+		$('.menuleft a span').css('left', '5px');
+	} else {
+		$('.menuleft').css('left', '10%');
+		$('.menuleft a span').css('left', '12px');
+	}
+	if (height < 230 || width < 110) {
+		$('.menuleft a').css('font-size', '14px');
+		$('.menuleft a').css('height', '25px');
+	} else {
+		$('.menuleft a').css('font-size', '18px');
+		$('.menuleft a').css('height', '29px');
+	}
+	$('.menuleft a').css('width', '' + width + 'px');
+
 	// for top menu
 	if (width < 650) {
 		$('.menutop div.part').css('display', 'block');
 	} else {
 		$('.menutop div.part').css('display', 'inline');
 	}
+	if (width < 350) {
+		$('.menutop a').css('font-size', '12px');
+		$('.menutop a').css('margin-right', '15px');
+		$('.menutop').css('margin-left', '5px');
+	} else {
+		$('.menutop a').css('font-size', '15px');
+		$('.menutop a').css('margin-right', '30px');
+		$('.menutop').css('margin-left', '15px');
+	}
 }
-
 
 $(window).resize(function() {
 	adjustStyle($(this).width(), $(this).height());
@@ -24,7 +49,7 @@ $(window).resize(function() {
 
 $(document).ready(function() {
 
-	adjustStyle($(this).width(), $(this).height());
+	adjustStyle($(window).width(), $(window).height());
 
 	var menu = $('.menu'), a = menu.find('a');
 
@@ -36,6 +61,5 @@ $(document).ready(function() {
 		t.find('span').toggleClass('highlight');
 		s.find('span').toggleClass('blur');
 	});
-
 
 });
--- naturheilkunde-praxis/web/javascript/resolution.js
+++ naturheilkunde-praxis/web/javascript/resolution.js
@@ -1,39 +1,53 @@
-function adjustStyle(width,height) {
-    width = parseInt(width);
-    height = parseInt(height);
-    var innerhframe=document.getElementById('innerhframe');
-    var outerhframe=document.getElementById('outerhframe');
-    var outervframe=document.getElementById('outervframe');
-    var toplogoframe=$(top.toplogoframe.document);
-    var bottomonlineframe=$(top.bottomonlineframe.document);
-    if(innerhframe && outerhframe && outervframe && toplogoframe && bottomonlineframe) {
-        if(width<1100 || height < 700) {
-        	var logoheight=Math.min(width/11, Math.min(width/20, 60)+height/20);
-        	var logomargin=Math.min(width/50, height/50);
-        	outervframe.rows=logoheight+',*,55';
-        	toplogoframe.find('body').css('margin-left', '5px');
-        	toplogoframe.find('body').css('margin-top', ''+logomargin);
-        	bottomonlineframe.find('*').addClass('small');
-        }
-        else {
-        	outervframe.rows='120,*,95';
-        	toplogoframe.find('body').css('margin-left', '30px');
-        	toplogoframe.find('body').css('margin-top', '30px');
-        	bottomonlineframe.find('*').removeClass('small');
-        }
-        if(width<1100) {
-        	toplogoframe.find('img').css('width', '100%');
-        }
-        else {
-        	toplogoframe.find('img').css('width', '844');
-        }
-    	outerhframe.cols=(width>=1200)?'15%,73%,12%':'15%,80%,5%';
-    	innerhframe.cols=(width>=1200)?'18%,82%':'125,*';
-    }
+function adjustStyle(width, height) {
+	width = parseInt(width);
+	height = parseInt(height);
+	var innerhframe = document.getElementById('innerhframe');
+	var outerhframe = document.getElementById('outerhframe');
+	var outervframe = document.getElementById('outervframe');
+	var toplogoframe = $(top.toplogoframe.document);
+	var bottomonlineframe = $(top.bottomonlineframe.document);
+	if (innerhframe && outerhframe && outervframe && toplogoframe
+			&& bottomonlineframe) {
+		// sizing of top and bottom
+		if (width < 1100 || height < 700) {
+			var logoheight = Math.min(width / 11, Math.min(width / 20, 60)
+					+ height / 20);
+			var logomargin = Math.min(width / 50, height / 50);
+			outervframe.rows = logoheight + ',*,55';
+			toplogoframe.find('body').css('margin-left', '5px');
+			toplogoframe.find('body').css('margin-top', '' + logomargin);
+			bottomonlineframe.find('*').addClass('small');
+		} else {
+			outervframe.rows = '120,*,95';
+			toplogoframe.find('body').css('margin-left', '30px');
+			toplogoframe.find('body').css('margin-top', '30px');
+			bottomonlineframe.find('*').removeClass('small');
+		}
+
+		if (width < 1100) {
+			toplogoframe.find('img').css('width', '100%');
+		} else {
+			toplogoframe.find('img').css('width', '844');
+		}
+
+		// horizontal sizing
+		if (width > 1200) {
+			outerhframe.cols = '15%,73%,12%';
+			innerhframe.cols = '18%,82%';
+		} else {
+			if (width > 600) {
+				innerhframe.cols = '125,*';
+				outerhframe.cols = '15%,80%,5%';
+			} else {
+				innerhframe.cols = '100,*';
+				outerhframe.cols = '10%,90%,0';
+			}
+		}
+	}
 }
 
 function adjustMainStyle() {
-    adjustStyle($(this).width(),$(this).height());
+	adjustStyle($(this).width(), $(this).height());
 }
 
 $(window).resize(function() {
@@ -43,4 +57,3 @@ $(window).resize(function() {
 $(document).ready(function() {
 	adjustMainStyle();
 });
-
