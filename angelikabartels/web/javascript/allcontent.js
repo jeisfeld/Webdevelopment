78231e2f77de562d5eb5fe2dc6e0afa9ee546391
Jörg Eisfeld <github@jeisfeld.de>
2015-01-25 13:49:55 +0100

    Ensure that print dialog is only opened after all pages and images are loaded.

--- angelikabartels/web/javascript/allcontent.js
+++ angelikabartels/web/javascript/allcontent.js
@@ -1,6 +1,35 @@
+var pagesLoaded = 0;
+var imagesLoaded = 0;
+var totalPages = 0;
+var totalImages = 0;
+
+function imageLoaded() {
+	imagesLoaded++;
+	if (imagesLoaded == totalImages) {
+		window.print();
+	}
+}
+
 function loadPage() {
 	var path = $(this).attr('path');
-	$(this).load(path);
+	$(this).load(path, function(responseData) {
+		pagesLoaded += 1;
+
+		if (pagesLoaded == totalPages) {
+			var images = $(document).find('img');
+			totalImages = images.size();
+
+			images.each(function() {
+				if (this.complete) {
+					imageLoaded.call(this);
+				} else {
+					$(this).one('load', imageLoaded);
+				}
+			});
+
+			totalImages = $(document).find('img').size();
+		}
+	});
 }
 
 $(document).ready(function() {
@@ -11,10 +40,10 @@ $(document).ready(function() {
 		},
 	});
 
-	$('#allcontent div.load').each(loadPage);
+	var pagesToBeLoaded = $('#allcontent div.load');
+	totalPages = pagesToBeLoaded.size();
+
+	pagesToBeLoaded.each(loadPage);
 
 	$('#pageloading').remove();
-
-	$('#allcontent div.loadfinally').each(loadPage);
-
 });
