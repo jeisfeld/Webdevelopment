1d3104994684e0266f85b10689e4ccefb0da7a17
Jörg Eisfeld <github@jeisfeld.de>
2015-03-25 21:49:45 +0100

    ITA: created top logo and bottom

---
+++ it-art/web/javascript/resolution.js
@@ -0,0 +1,156 @@
+function needsMobileAdaptation() {
+	return window.devicePixelRatio > 1
+			&& /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
+
+function adjustStyle(width, height) {
+	width = parseInt(width);
+	height = parseInt(height);
+	var mainframe = $('#mainframe');
+	var menuleftframe = $('#menuleftframe');
+	var menudropdownframe = $('#menudropdownframe');
+	var toplogoframe = $('#toplogoframe');
+	var bottomframe = $('#bottomframe');
+	var popupframe = $('#popupframe');
+	var indexbody = $('#index');
+	var toplogocontent = toplogoframe.contents().find('#logoimage');
+	var mainimages = mainframe.contents().find('#text img');
+	var bottombody = bottomframe.contents().find('body');
+
+	// sizing of top and bottom
+	var bottomheight = 50;
+	if (width < 1000) {
+		var limitedwidth = width;
+		toplogocontent.addClass('fullwidth');
+	} else {
+		var limitedwidth = 1240;
+		toplogocontent.removeClass('fullwidth');
+	}
+
+	if (width < height || width < 1000) {
+		toplogocontent.attr('src', 'img/logo-it-art_narrow.png');
+		var logoheight = limitedwidth * 200 / 1000;
+		toplogocontent.addClass('fullwidth');
+	} else if (width < 1400) {
+		toplogocontent.attr('src', 'img/logo-it-art_narrow.png');
+		toplogocontent.removeClass('fullwidth');
+		var logoheight = 200;
+	} else {
+		toplogocontent.attr('src', 'img/logo-it-art_wide.png');
+		toplogocontent.removeClass('fullwidth');
+		var logoheight = 200;
+	}
+	var middleheight = height - logoheight - bottomheight;
+	toplogoframe.css('height', logoheight);
+
+	mainframe.css('top', logoheight);
+	mainframe.css('height', middleheight);
+	menuleftframe.css('top', logoheight);
+	menuleftframe.css('height', middleheight);
+
+	bottomframe.css('top', height - 50);
+
+	// background positioning - image is square!
+
+	if (width < height) {
+		var offset = (width - height) / 2
+		var positionString = '' + offset + 'px 0px';
+		var sizeString = '' + height + 'px ' + height + 'px';
+	} else {
+		var offset = (height - width) / 2
+		var positionString = '0px ' + offset + 'px';
+		var sizeString = '' + width + 'px ' + width + 'px';
+	}
+
+	indexbody.css('background-position', positionString);
+	indexbody.css('background-size', sizeString);
+
+	// footer text sizing
+	if (width < 620) {
+		bottombody.addClass('smaller');
+		bottombody.removeClass('small');
+	} else if (width < 800) {
+		bottombody.addClass('small');
+		bottombody.removeClass('smaller');
+	} else {
+		bottombody.removeClass('small');
+		bottombody.removeClass('smaller');
+	}
+
+	// hide menu on narrow screens.
+	if (width < 850 || needsMobileAdaptation()) {
+		menuleftframe.hide();
+		bottomframe.hide();
+		mainframe.css('height', height - logoheight);
+
+		menudropdownframe.show();
+		menudropdownframe.css('top', logoheight - 24);
+		mainframe.css('left', 0);
+		mainframe.css('width', '100%');
+
+		// limit image size
+		mainimages.css('max-width', width * 0.5);
+
+		// impressum appears on main window
+		popupframe.css('top', logoheight);
+		popupframe.css('height', height - logoheight);
+		popupframe.css('left', 0);
+		popupframe.css('width', '100%');
+		popupframe.css('border', 'none');
+	} else {
+		menuleftframe.show();
+		bottomframe.show();
+		mainframe.css('height', middleheight);
+
+		menudropdownframe.hide();
+		mainframe.css('left', '21%');
+		mainframe.css('width', '79%');
+
+		// limit image size
+		mainimages.css('max-width', width * 0.4);
+
+		// impressum appears in popupframe
+		popupframe.css('top', height / 4);
+		popupframe.css('height', height / 2);
+		popupframe.css('left', width / 4);
+		popupframe.css('width', width / 2);
+		popupframe.css('border', '2px solid black');
+	}
+}
+
+function adjustMainStyle() {
+	if (!$.support.leadingWhitespace) {
+		adjustForIE8();
+	}
+	adjustStyle($(window).width(), $(window).height());
+	adjustStyle($(window).width(), $(window).height());
+}
+
+function adjustForIE8() {
+	$('#index').addClass('ie8');
+	$('#bottomframe').contents().find('body').addClass('ie8');
+	$('#mainframe').contents().find('body').addClass('ie8');
+	$('#menuleftframe').contents().find('body').addClass('ie8');
+	$('#toplogoframe').contents().find('body').addClass('ie8');
+	$('#popupframe').contents().find('body').addClass('ie8');
+}
+
+$(window).resize(function() {
+	adjustMainStyle();
+});
+
+$(document).ready(function() {
+	$('#toplogoframe').on("load", function() {
+		adjustMainStyle();
+	});
+
+	$('#mainframe').on("load", function() {
+		adjustMainStyle();
+	});
+
+	$('#bottomframe').on("load", function() {
+		adjustMainStyle();
+	});
+
+	adjustMainStyle();
+});
