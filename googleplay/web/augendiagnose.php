c53a6692aa585fd2676cfcd2ab386cc61a39c35e
Jörg Eisfeld <github@jeisfeld.de>
2015-05-01 21:03:56 +0200

    Use separate project for augendiagnose instead of googleplay

--- googleplay/web/augendiagnose.php
+++ googleplay/web/augendiagnose.php
@@ -3,17 +3,17 @@ include 'Mobile_Detect.php';
 $detect = new Mobile_Detect;
 if ($detect->isMobile()) {
 	if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "de"))
-	{ header("Location: de/index_m.html"); }
+	{ header("Location: http://augendiagnose.jeisfeld.de/de/index_m.html"); }
 	else if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "es"))
-	{ header("Location: es/index_m.html"); }
+	{ header("Location: http://augendiagnose.jeisfeld.de/es/index_m.html"); }
 	else
-	{ header("Location: en/index_m.html"); }
+	{ header("Location: http://augendiagnose.jeisfeld.de/en/index_m.html"); }
 } else {
 	if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "de"))
-	{ header("Location: de/index.html"); }
+	{ header("Location: http://augendiagnose.jeisfeld.de/de/index.html"); }
 	else if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "es"))
-	{ header("Location: es/index.html"); }
+	{ header("Location: http://augendiagnose.jeisfeld.de/es/index.html"); }
 	else
-	{ header("Location: en/index.html"); }
+	{ header("Location: http://augendiagnose.jeisfeld.de/en/index.html"); }
 }
 ?>
