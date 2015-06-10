d44670930c38bf51dd267c246859c3161a422709
Jörg Eisfeld <github@jeisfeld.de>
2015-06-11 01:04:21 +0200

    Fixed redirect from googleplay

--- googleplay/web/augendiagnose.php
+++ googleplay/web/augendiagnose.php
@@ -1,19 +1,3 @@
 <?PHP
-include 'Mobile_Detect.php';
-$detect = new Mobile_Detect;
-if ($detect->isMobile()) {
-	if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "de"))
-	{ header("Location: http://augendiagnose.jeisfeld.de/de/index_m.html"); }
-	else if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "es"))
-	{ header("Location: http://augendiagnose.jeisfeld.de/es/index_m.html"); }
-	else
-	{ header("Location: http://augendiagnose.jeisfeld.de/en/index_m.html"); }
-} else {
-	if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "de"))
-	{ header("Location: http://augendiagnose.jeisfeld.de/de/index.html"); }
-	else if (strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], "es"))
-	{ header("Location: http://augendiagnose.jeisfeld.de/es/index.html"); }
-	else
-	{ header("Location: http://augendiagnose.jeisfeld.de/en/index.html"); }
-}
+header("Location: http://augendiagnose.jeisfeld.de");
 ?>
