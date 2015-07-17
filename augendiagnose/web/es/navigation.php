01d6d05b9756772d5bc0887f2122d79e02c74d12
Jörg Eisfeld <github@jeisfeld.de>
2015-07-17 16:42:26 +0200

    AUG: Update link to spanish donation page

--- augendiagnose/web/es/navigation.php
+++ augendiagnose/web/es/navigation.php
@@ -47,11 +47,12 @@ else {
 	</p>
 
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-		<input type="hidden" name="cmd" value="_s-xclick"> <input type="hidden" name="hosted_button_id" value="RUB5VFVSETVMN">
-		<input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_donate_LG.gif" border="0" name="submit"
-			alt="PayPal, la forma más segura y rápida de pagar en línea."> <img alt="" border="0"
+		<input type="hidden" name="cmd" value="_s-xclick"> <input type="hidden" name="hosted_button_id" value="BUNWSRJGY3D98">
+		<input type="image" src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_donate_LG.gif" border="0" name="submit"
+			alt="PayPal. La forma rápida y segura de pagar en Internet."> <img alt="" border="0"
 			src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
 	</form>
+
 
 </body>
 
