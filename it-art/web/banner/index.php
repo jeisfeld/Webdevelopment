3eb3551941f1a1360f543e850c693d73b692ca21
Jörg Eisfeld <github@jeisfeld.de>
2015-07-23 23:53:31 +0200

    ITA: In Germany, route banner to it-art.de

--- it-art/web/banner/index.php
+++ it-art/web/banner/index.php
@@ -1,4 +1,10 @@
 <?php
-header("Location: http://augendiagnose.jeisfeld.de");
-exit;
+if (strstr ( $_SERVER ['HTTP_ACCEPT_LANGUAGE'], "de" )) {
+	header ( "Location: http://it-art.de" );
+	exit ();
+}
+else {
+	header ( "Location: http://augendiagnose.jeisfeld.de" );
+	exit ();
+}
 ?>
