638e51391fedd7a201f1c2247bc58bb8b9215b90
Jörg Eisfeld <github@jeisfeld.de>
2016-10-03 12:48:16 +0200

    Enabled Google Analytics for NHP, TZD, AB

--- angelikabartels/web/javascript/subpage.js
+++ angelikabartels/web/javascript/subpage.js
@@ -1,6 +1,6 @@
 /**
  * Utility method to mark the menu entry of the current page as "chosen".
- *
+ * 
  * @param frame
  * @param id
  * @returns {Boolean}
@@ -21,9 +21,23 @@ $(document).ready(function() {
 	var myId = $('body').attr('id');
 	var frame, result;
 
-
-	if(top.menu != null && top.menu.document != null) {
+	if (top.menu != null && top.menu.document != null) {
 		frame = top.menu.document;
 		markChosen(frame, myId);
 	}
 });
+
+// Google Analytics tracking
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-85063283-2', 'auto');
+ga('send', 'pageview');
--- naturheilkunde-praxis/web/javascript/subpage.js
+++ naturheilkunde-praxis/web/javascript/subpage.js
@@ -1,9 +1,9 @@
 function markChosen(frame, id) {
-	if(frame) {
+	if (frame) {
 		var t = $(frame.getElementById(id));
 		var s = $(frame).find('a');
-		//alert(t.html());
-		if(t) {
+		// alert(t.html());
+		if (t) {
 			s.removeClass('chosen');
 			t.addClass('chosen');
 		}
@@ -11,23 +11,36 @@ function markChosen(frame, id) {
 	return (t != null && t.html().toLowerCase().match('span'));
 }
 
-
 $(document).ready(function() {
-	var myId=$('body').attr('id');
+	var myId = $('body').attr('id');
 	var frame, result;
-	
+
 	// first try in top frame
 	try {
 		frame = top.main.menutop.document;
 		result = markChosen(frame, myId);
-	}
-	catch (e) {
+	} catch (e) {
 	}
 
 	// otherwise, try in left frame
-	if(!result) {
+	if (!result) {
 		frame = top.menu.document;
 		markChosen(frame, myId);
 	}
-	
+
 });
+
+//Google Analytics tracking
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-85063283-1', 'auto');
+ga('send', 'pageview');
