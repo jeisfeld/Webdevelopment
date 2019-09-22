4882eb7a609748afe067c31b7954bb58185dfe4f
Jörg Eisfeld <github@jeisfeld.de>
2019-09-22 15:16:05 +0200

    AUG: Put DOCTYPE before analytics script

--- augendiagnose/web/de/organize_photos.php
+++ augendiagnose/web/de/organize_photos.php
@@ -1,9 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <?PHP
 include "pageheader.php";
 
 if (isAugendiagnose ()) {
 	?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title><?=$appname?> - Fotos organisieren</title>
--- augendiagnose/web/pt/overview.php
+++ augendiagnose/web/pt/overview.php
@@ -1,7 +1,7 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <?PHP
 include "pageheader.php";
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title><?=$appname?> - Informações Gerais</title>
