79f95d551807f51a069566a05c614c37d18a155b
Jörg Eisfeld <github@jeisfeld.de>
2022-04-26 17:00:58 +0200

    ITA: updated music page

--- it-art/web/subpages/musik.php
+++ it-art/web/subpages/musik.php
@@ -1,35 +1,9 @@
 <h1>Musik</h1>
 
-<p>Ich erstelle Aufnahmen von Musikstücken und übernehme dabei, je nach Bedarf, Komposition,
-	Textgestaltung, Instrumentierung, Arrangement und Mastering.</p>
+<p>Ich mache Musik - meist mit Fokus Gesang und Gitarre: Covern bekannter Songs, umgetextete Versionen anlässlich Jubiläen,
+Vertonungen von Lyrik, oder eigene Songs.</p>
 
-<p>Bisher gibt es nur Aufnahmen aus privaten Anlässen - einerseits Aufnahmen mit individuellen Texten für
-	Familienfeiern, andererseits diverse Aufnahmen unterschiedlicher Art aus eigenem Antrieb.</p>
-
-<p>Hörbeispiele:</p>
-<table class="bordered audiotable">
-	<tr>
-		<td class="audiolabel1">Variation eines klassischen Themas:</td>
-		<td class="audiowithlabel">
-			<div class="audiolabel2">Variation eines klassischen Themas:</div>
-			<div class="audio">
-				<audio controls autoplay id="zarathustra">
-					<source src="<?=$basepath?>/mp3/zarathustra1.mp3" type="audio/mpeg" />
-				</audio>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td class="audiolabel1">Pop-Song:</td>
-		<td class="audiowithlabel">
-			<div class="audiolabel2">Pop-Song:</div>
-			<div class="audio">
-				<audio controls id="dreamalittledream">
-					<source src="<?=$basepath?>/mp3/woistsiegeblieben6.mp3" type="audio/mpeg" />
-				</audio>
-			</div>
-		</td>
-	</tr>
-</table>
+<p>Beispiele auf Youtube:</p>
+<p><a href="https://www.youtube.com/channel/UC7NfcPPjdBcTkSRwe3qFaeQ" target="_blank"><img src="<?=$basepath?>/img/youtube.jpg"></a></p>
 
 <img class="centered" src="<?=$basepath?>/img/clef.jpg" />
