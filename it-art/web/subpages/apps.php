b6ed34d370d2d8441213732b3b6c9216a44362a1
Jörg Eisfeld <github@jeisfeld.de>
2022-09-17 10:44:45 +0200

    ITA: Added reference to LIFX Tools

--- it-art/web/subpages/apps.php
+++ it-art/web/subpages/apps.php
@@ -3,14 +3,16 @@
 <p>Als kleine Helferlein, etwa für den praktischeren Zugang zu Webinhalten vom Smartphone, kann ich auch individuelle
 	Android-Apps entwickeln.</p>
 
-<p>Zwei von mir erstellte Apps:</p>
+<p>Drei von mir erstellte Apps:</p>
 <p>
 	<a href="https://play.google.com/store/apps/details?id=de.jeisfeld.randomimage" target="_blank" class="image">
 		<img alt="Randomimage" src="<?=$basepath?>/img/randomimage.jpg" />
 	</a>
-	&nbsp;
 	<a href="https://play.google.com/store/apps/details?id=de.eisfeldj.augendiagnose" target="_blank" class="image">
 		<img alt="Augendiagnose" src="<?=$basepath?>/img/augendiagnose.jpg" />
+	</a>
+	<a href="https://play.google.com/store/apps/details?id=de.jeisfeld.lifx.app" target="_blank" class="image">
+		<img alt="Augendiagnose" src="<?=$basepath?>/img/lifxtools.jpg" />
 	</a>
 </p>
 <p>Hinweis: Ich baue nur Apps für Android, nicht für iPhones oder Windows Phone.</p>
