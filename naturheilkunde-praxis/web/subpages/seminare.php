3e3520d7a13579fec023a20b939d82d266e8750e
Jörg Eisfeld <github@jeisfeld.de>
2024-08-14 17:19:29 +0200

    NHP: Feedback

--- naturheilkunde-praxis/web/subpages/seminare.php
+++ naturheilkunde-praxis/web/subpages/seminare.php
@@ -23,10 +23,78 @@
 
 <p>
 	Im <b>Essenz-Seminar II</b> wird das Grundlagen-Wissen der TCM vertieft. Außerdem wird die <b>TEM
-	(tradi&shy;tio&shy;nel&shy;le euro&shy;pä&shy;ische Medizin)</b> mit den Mine&shy;ralien, der Psycho-Somatik und den
+		(tradi&shy;tio&shy;nel&shy;le euro&shy;pä&shy;ische Medizin)</b> mit den Mine&shy;ralien, der Psycho-Somatik und den
 	Test&shy;methoden erläutert. Die indi&shy;vidu&shy;ellen Bedürf&shy;nisse und das Finden und Testen der idealen Mittel
 	werden eine zentrale Rolle spielen.
 </p>
 
 <p>Mein Ziel ist es, Sie auf dem Weg zu mehr Gesund&shy;heit, Vitalität und Lebens&shy;freude zu unter&shy;stützen, um
 	gesünder, bewusster und zufriedener durchs Leben zu gehen.</p>
+
+<h3>Feedback von Teilnehmerinnen</h3>
+
+<ul class="feedback">
+	<li><span class="feedback-text">Sybille Schraml ist Heilpraktikerin aus Leidenschaft. Empathisch, kompetent und
+			authentisch.<br /> Was sie so besonders macht: Erfrischend undogmatisch bringt sie verschiedene Analyse-Methoden der
+			TCM und der TEM zusammen.<br /> Durch diesen Methoden-Pool erreicht sie eine konkrete und zielführende
+			Behandlungsempfehlung, die ihresgleichen sucht.<br /> In dem Seminar "Gesundheit, Vitalität & Lebensfreude" gibt
+			Sybille Schraml einen laiengerechten und praktischen Einblick in dieses fundierte Wissen. Erklärtes Ziel: Erkenne die
+			Signale deines Körpers, deute sie richtig und handle rechtzeitig präventiv.<br />
+			<ul>
+				<li>Du tauchst ein in das Grundprinzip der 5-Elemente Lehre der chinesischen Medizin.</li>
+				<li>Du erkennst Zusammenhänge im Körper, die dir bisher unbekannt waren, nach dem 5-Elemente Kreislauf aber logisch
+					werden.</li>
+				<li>Du bekommst einen völlig neuen Blick darauf, was die Signale des Körpers bedeuten.</li>
+				<li>Du übst eine praktische Testmethode mit Solunaten zur präventiven Selbstmedikation.</li>
+			</ul> Fazit: Zwei Tage staunen, lernen, verstehen, üben, Spaß haben und eine konkrete, praktische Anleitung zur
+			Selbsthilfe mit nach Hause nehmen.</br>Definitiv empfehlenswert!
+	</span> <span class="feedback-author">(U.W.)</span></li>
+	<li><span class="feedback-text">Besonders gut gefallen hat mir:
+			<ul>
+				<li>Die Zusammenhänge in unserem Körper kennenzulernen besonders im Hinblick auf das große Ganze</li>
+				<li>Es war „nur“ ein Schnuppern aber es hat mich sehr beeindruckt</li>
+				<li>Alternativen zur Schulmedizin zu kennen</li>
+				<li>Bereitschaft auf seinen Körper zu hören</li>
+				<li>Die Trainerin: das umfassende Wissen und die liebe Art und Geduld es zu vermitteln</li>
+				<li>Die Teilnehmer, die alle offen waren für was neues</li>
+			</ul> Besonders hilfreich fand ich im zweiten Seminar das Kennenlernen der Organuhr und die ersten Versuche mit dem
+			Pendeln. Da brauche ich noch mehr Sicherheit, aber der Anfang ist gemacht.<br /> Zum Schluss bleibt nur, es war alles
+			in allem ein sehr gelungenes Seminar. Ich habe mir mittlerweile alle Bücher angeschafft und wende sie auch an.
+	</span><span class="feedback-author">(A.U.)</span></li>
+	<li><span class="feedback-text">„Gesundheit, Vitalität & Lebensfreude“ - bereits im Titel erkennt man den ‚Fokus aufs
+			Gute‘ und die ‚Empathie‘, mit denen Sybille Schraml ihre Heilkunst ausübt. Und genau so verlaufen die beiden
+			Seminartage auch: mit Kompetenz und Leidenschaft vermittelt Sybille Schraml Grundlagen, die Dir helfen die Signale
+			Deines Körpers zu erkennen und zu beachten. Die wichtigsten Zusammenhänge der traditionellen chin. Medizin werden
+			anschaulich vermittelt. Und das Allerbeste: man geht nach Hause mit einem kleinen Werkzeugkoffer, der einem hilft die
+			Selbstheilung des eigenen Körpers zu unterstützen. Unbedingt empfehlenswert.</span> <span class="feedback-author">(B.G.)</span></li>
+	<li><span class="feedback-text">Ich beschäftige mich seit ca 30 Jahren mit alternativer Medizin / Heilmethoden.
+			Speziell meine Kenntnisse der 5 Biologischen Naturgesetze nach Dr. Hamer konnte ich im Laufe der Zeit immer besser
+			mit der TCM kombinieren, die Testergebnisse nachvollziehen und die entsprechenden Heilmittel anwenden.<br /> Die
+			Seminare haben mein Wissen weiter vertieft und ein besseres Verständnis für TCM und die Pulstestung der am besten
+			passenden Heilmittel gebracht. Besonders beeindruckt hat mich Frau Schramls Zeit bei Dr. Sachse, der diese Testmethode
+			der Pulstestung in akribischer Kleinarbeit über viele Jahre bis zu seinem Tod erforscht und weiterentwickelt hat und
+			bei dem sie persönlich lernen und assistieren durfte.<br /> Sehr wertvoll sind für mich die Solunate nach Alexander
+			von Bernus, die seit über 100 Jahren Bestandteil der naturheilkundlichen Praxis sind. Ich habe mich aufgrund des
+			Seminars noch intensiver mit den Solunaten befasst und war beeindruckt, mit welcher Achtung diese Heilmittel nach wie
+			vor hergestellt werden.<br /> Danke an Sybille Schraml und an alle Seminarteilnehmerinnen für das vertrauensvolle
+			Miteinander.
+	</span> <span class="feedback-author">(T.W.)</span></li>
+	<li><span class="feedback-text">Das Seminar hat mir einen sehr guten Rundumblick erbracht über TCI, Testmethoden und
+			Solunat-Produkte. Für mich war es eine große Bereicherung für mein Leben.<br> Besonders gut gefallen hat mir an dem
+			Seminar:
+			<ul>
+				<li>sehr gute, leicht zu merkende Erklärungen</li>
+				<li>gelungene Zusammenfassung eines großen Themenkomplexes</li>
+				<li>viele Querbezüge, unkompliziert dargestellt</li>
+				<li>ständige Anregung zur Selbstreflexion und zum Austausch</li>
+				<li>regelmäßige Vertiefungseinheiten, damit das eben Erlernte wirklich verstanden werden kann</li>
+				<li>sehr gute Wiederholungen, damit das Erlernte gefestigt werden kann</li>
+				<li>viel Austausch in der Gruppe über das soeben Erlernte</li>
+				<li>große Offenheit aller Fragen der Teilnehmer</li>
+				<li>sehr angenehme, durchwegs herzliche und freundliche Atmosphäre die ganzen Tage über</li>
+				<li>absoluter Respekt jeder teilnehmenden Person gegenüber</li>
+			</ul> Am hilfreichsten war für mich die Übersicht über die 5 Elemente TCI und dann damit die konkrete Verknüpfung zu
+			Therapiemöglichkeiten und Testmöglichkeiten.
+	</span><span class="feedback-author">(anonym)</span></li>
+
+</ul>
