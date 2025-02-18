226eb37d1c0734f989992162b910ca6030b48e8a
Jörg Eisfeld <github@jeisfeld.de>
2025-02-19 00:46:42 +0100

    JEI: Improved search

--- jeisfeld/web/songarchive/search.php
+++ jeisfeld/web/songarchive/search.php
@@ -14,6 +14,8 @@ if ($query === "") {
 
 // Use regex to check if query is a valid ID (numeric OR numeric + single letter)
 $is_valid_id = preg_match ( '/^\d+[a-zA-Z]?$/', $query );
+$isSingleLetter = preg_match ( '/^[a-zA-Z]$/', $query );
+
 error_log ( "Query string: " . $query );
 
 // SQL Query
@@ -28,6 +30,18 @@ if ($is_valid_id) {
 else if ($query === "*") {
 	$sql = "SELECT id, title, text, tabfilename, mp3filename, author FROM songs order by id";
 }
+else if ($isSingleLetter) {
+	$sql = "SELECT id, title, text, author, tabfilename, mp3filename FROM songs
+            WHERE title REGEXP CONCAT('(^| )', ?, '.*')
+               OR text REGEXP CONCAT('(^| )', ?, '.*')
+            ORDER BY CASE WHEN title REGEXP CONCAT('(^| )', ?, '.*') THEN 1 ELSE 2 END, CAST(id AS UNSIGNED) ASC";
+	$params = [ 
+			$query,
+			$query,
+			$query
+	];
+	$types = "sss";
+}
 else {
 	// If searching by title/text
 	$words = explode ( " ", $query );
@@ -37,18 +51,28 @@ else {
 
 	foreach ( $words as $word ) {
 		$conditions [] = "(title LIKE ? OR text LIKE ? OR author like ? OR keywords like ?)";
+		$titleMatch [] = "IF(title LIKE ?, 1, 0)";
+		$textMatch [] = "IF(text LIKE ?, 1, 0)";
+
 		$params [] = "%" . $word . "%";
 		$params [] = "%" . $word . "%";
 		$params [] = "%" . $word . "%";
 		$params [] = "%" . $word . "%";
-		$types .= "ssss";
+		$params [] = "%" . $word . "%"; // Condition for ORDER BY (Title Match)
+		$params [] = "%" . $word . "%"; // Condition for ORDER BY (Text Match)
+
+		$types .= "ssssss";
 	}
 
-	$sql = "SELECT id, title, text, tabfilename, mp3filename, author FROM songs WHERE " . implode ( " AND ", $conditions ) . "
-            ORDER BY CASE WHEN title LIKE ? THEN 1 WHEN text LIKE ? THEN 2 ELSE 3 END, CAST(id AS UNSIGNED) ASC";
-	$params [] = "%" . $query . "%";
-	$params [] = "%" . $query . "%";
-	$types .= "ss";
+	$sql = "SELECT id, title, text, tabfilename, mp3filename, author,
+                (" . implode(" + ", $titleMatch) . ") AS title_match_count,
+                (" . implode(" + ", $textMatch) . ") AS text_match_count
+            FROM songs
+            WHERE " . implode(" AND ", $conditions) . "
+            ORDER BY
+                title_match_count DESC,  
+                text_match_count DESC,
+                CAST(id AS UNSIGNED) ASC";
 }
 
 // Execute SQL
