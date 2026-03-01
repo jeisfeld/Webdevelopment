c605b207f32ebe18573aa4aeac9a097b9d4c45f4
Jörg Eisfeld <github@jeisfeld.de>
2026-03-01 13:48:15 +0100

    NHP: Update uebermich

--- naturheilkunde-praxis/web/subpages/uebermich.php
+++ naturheilkunde-praxis/web/subpages/uebermich.php
@@ -8,22 +8,28 @@
 	<li>Hakomi Interpersonal - Schwerpunkt Beziehung 2004</li>
 	<li>Trauma Therapie bei Manuela Mischke-Reeds (nach Peter Levine) seit 2013</li>
 	<li>Iridologie (Augen&shy;diagnose) Felke Institut seit 2015</li>
-	<li>Ignazianische Schweigeexerzitien 2019</li>
-	<li>Spiritualität und Medizin 2020</li>
+	<li>Ignazianische Schweigeexerzitien 2017-2019</li>
 	<li>Blog &bdquo;<a href="http://aelter-werden.eu" target="_blank">Älter werden</a>&ldquo; 2020
 	</li>
 	<li><a href="https://www.alegria-lebensfreude.de/" target="_blank">&bdquo;Alegria&ldquo;</a> Netzwerk für Lebensfreude,
 		Gesundheit &amp; Vitalität 2021</li>
-	<li><a href="/seminare">Seminare:</a> Die Weitergabe meiner Essenz von 40 Jahren naturheilkundlicher Erfahrung und
-		Behandlung 2023/2024</li>
+	<li>Spiritualität und Seelsorge (Pfarrgemeinderat seit 2018, <a
+			href="https://www.muenchen.tv/mediathek/video/die-katholische-kirche-bringt-den-aschermittwoch-zu-den-menschen/"
+			target="_blank">Dekanatsrat</a> seit 2022)
+	
+	<li><a href="/seminare">Seminare:</a> Die Weitergabe meiner Essenz von 40 Jahren natur&shy;heil&shy;kundlicher
+		Erfahrung und Behandlung seit 2024</li>
+
+
+
 </ul>
 
-<p>Bereits 1984 begann mein natur&shy;heilkundlich geprägter Weg. Als Assistentin bei dem Natur&shy;heilarzt Dr. Seemann
-	in Aalen lernte ich damals Bioresonanz, Elektro&shy;akupunktur nach Voll (EAV), Farblicht-Therapie nach Peter Mandel
-	und weitere alternativ medizinische Verfahren kennen. Nach 4 Jahren führte mein Weg 1988 zur 3 jährigen Ganztags
-	Heil&shy;praktiker Schule nach München. Während dieser Zeit assistierte ich bei dem Natur&shy;heilarzt Dr. Sachsse bis
-	1997. Sein Schwerpunkt war die Bioresonanz, Allergie und seine Testmethode der Pulsreflex nach Nogier, die ich bis
-	heute erfolgreich anwende.</p>
+<p>Von 1980-1984 war ich Arzthelferin in einer Landarztpraxis. Danach begann 1984 mein natur&shy;heil&shy;kundlich
+	geprägter Weg. Als Assistentin bei dem Natur&shy;heilarzt Dr. Seemann in Aalen lernte ich damals Bioresonanz,
+	Elektro&shy;akupunktur nach Voll (EAV), Farblicht-Therapie nach Peter Mandel und weitere alternativ medizinische
+	Verfahren kennen. Nach 4 Jahren führte mein Weg 1988 zur 3 jährigen Ganztags Heil&shy;praktiker Schule nach München.
+	Während dieser Zeit assistierte ich bei dem Natur&shy;heilarzt Dr. Sachsse bis 1997. Sein Schwerpunkt war die
+	Bioresonanz, Allergie und seine Testmethode der Pulsreflex nach Nogier, die ich bis heute erfolgreich anwende.</p>
 <p>Die Einflüsse dieser beiden Ärzte/Mentoren, die bis zu ihrem letzten Atemzug in der Praxis tätig waren, prägen meine
 	Arbeit bis heute. Meine persönlichen Schwerpunkte, die Chinesische Medizin, Psycho&shy;somatik und die Spagyrik flossen
 	mit in diese Arbeit ein. Letztere ist im tieferen Sinn eine Ordnungs&shy;therapie, die wie ein Magnet eine ordnende und
